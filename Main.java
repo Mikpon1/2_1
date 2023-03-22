d194524b704417f501ed9f3589fb95d2a718d44a
Mikpon1 <127319253+Mikpon1@users.noreply.github.com>
2023-03-22 13:50:52 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    // System.out.println("Hello World");
+   String napis = "Hello world!";
+    
+     for(int i=0; i<11; i++){
+       System.out.println(napis.charAt(i));
+     }
+  }
+}
